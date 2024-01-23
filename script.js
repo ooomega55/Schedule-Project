e303e7ee4b8bb73f8e2ff1b431770469200fdf85
ooomega55 <gvf55@outlook.com>
2024-01-23 13:42:36 -0500

    format fix

--- script.js
+++ script.js
@@ -1,37 +1,28 @@
 //wait for document to get ready
-$(document).ready( function () {
-    //get json file with getJSON method
-    $.getJSON("schedule.json", function (data) {
-        //store schedule data in variable
-        let scheduleData = data.schedule;
+$(document).ready(function () {
+  //get json file with getJSON method
+  $.getJSON("schedule.json", function (data) {
+    //store schedule data in variable
+    let scheduleData = data.schedule;
 
-        //populate table with schedule data
-        populateTable(scheduleData);
-    })
-    //function to populate table with data
-    function populateTable(scheduleData) {
-        $('#schedule-table-body').empty();
+    //populate table with schedule data
+    populateTable(scheduleData);
+  });
+  //function to populate table with data
+  function populateTable(scheduleData) {
+    $("#schedule-table-body").empty();
 
-        $.each(scheduleData, function (i, schedule) {
-            //create new row in table
-            let row = "<tr>";
-            //add class name, teacher name, room number, and days to row
-            row += "<td>" + schedule.class_name + "</td>"
-            row += "<td>" + schedule.teacher_name + "</td>";
-            row += "<td>" + schedule.room_number + "</td>";
-            row += "<td>" + schedule.days.join(', ') + "</td>";
-            row += "</td>"
-            //append the new row to the table body
-            $('#schedule-table-body').append(row)
-        })
-
-
-
-        
-        
-        
-
-        
-    }
-
-})
+    $.each(scheduleData, function (i, schedule) {
+      //create new row in table
+      let row = "<tr>";
+      //add class name, teacher name, room number, and days to row
+      row += "<td>" + schedule.class_name + "</td>";
+      row += "<td>" + schedule.teacher_name + "</td>";
+      row += "<td>" + schedule.room_number + "</td>";
+      row += "<td>" + schedule.days.join(", ") + "</td>";
+      row += "</td>";
+      //append the new row to the table body
+      $("#schedule-table-body").append(row);
+    });
+  }
+});
